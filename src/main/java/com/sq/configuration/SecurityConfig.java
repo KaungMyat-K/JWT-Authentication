f39cca61407397f8393a924c16f0b93fc3925454
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-04-07 23:03:49 +0630

    setup jwt

--- src/main/java/com/sq/configuration/SecurityConfig.java
+++ src/main/java/com/sq/configuration/SecurityConfig.java
@@ -1,10 +1,44 @@
 package com.sq.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.sq.service.userDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    
+    @Autowired
+    private userDetailsServiceImpl userDetailsServiceImpl;
+
+    SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+        http
+        .csrf(csrf->csrf.disable())
+        .userDetailsService(userDetailsServiceImpl)
+        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilter(null)
+        .authorizeHttpRequests(request->{
+            request.anyRequest().permitAll();
+        });
+        
+        return http.build();
+    }
+
+    @Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); 
+	}
+
+
     
 }
